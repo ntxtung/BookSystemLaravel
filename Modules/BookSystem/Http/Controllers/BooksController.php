cd3c1d433460f689928563eb6a5619a534d58484
Xuân Tùng <xuantung.1998@gmail.com>
2019-08-08 16:31:35 +0700

    Update

--- Modules/BookSystem/Http/Controllers/BooksController.php
+++ Modules/BookSystem/Http/Controllers/BooksController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Application\Http\Controllers;
+namespace Modules\BookSystem\Application\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 
 class BooksController extends Controller
 {
